ddfd58bf7655bf31ef842fe2182c6acaf09d0ec1
Lawrence M <39105751+manningli@users.noreply.github.com>
2021-04-13 21:12:24 -0400

    adding copy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 
@@ -9,6 +10,10 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'menu',
+    component: MenuComponent
   },
   {
     path: 'about',
---
+++ src/app/pages/my-story/my-story.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-story',
+  templateUrl: './my-story.component.html',
+  styleUrls: ['./my-story.component.scss']
+})
+export class MyStoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
