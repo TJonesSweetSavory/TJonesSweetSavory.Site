4f7b2b9367389a58cb03e54cb95090bf8430e755
Lawrence M <39105751+manningli@users.noreply.github.com>
2021-04-13 22:40:00 -0400

    adding copy (#4)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { AboutComponent } from './pages/about/about.component';
+import { MyStoryComponent } from './pages/my-story/my-story.component';
+import { MissionComponent } from './pages/mission/mission.component';
+import { VisionComponent } from './pages/vision/vision.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { AboutComponent } from './pages/about/about.component';
     FooterComponent,
     HomeComponent,
     ContactComponent,
-    AboutComponent
+    AboutComponent,
+    MyStoryComponent,
+    MissionComponent,
+    VisionComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/my-story/my-story.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyStoryComponent } from './my-story.component';
+
+describe('MyStoryComponent', () => {
+  let component: MyStoryComponent;
+  let fixture: ComponentFixture<MyStoryComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MyStoryComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MyStoryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
