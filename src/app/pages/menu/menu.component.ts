b3bc89299b43a08c7d43ec97e238ffb9840c888d
Lawrence M <39105751+manningli@users.noreply.github.com>
2025-07-11 18:40:47 -0400

    update to angular 19

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+    selector: 'app-menu',
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.scss'],
+    standalone: false
 })
 export class MenuComponent implements OnInit {
 
--- src/app/pages/mission/mission.component.ts
+++ src/app/pages/mission/mission.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-mission',
-  templateUrl: './mission.component.html',
-  styleUrls: ['./mission.component.scss']
+    selector: 'app-mission',
+    templateUrl: './mission.component.html',
+    styleUrls: ['./mission.component.scss'],
+    standalone: false
 })
 export class MissionComponent implements OnInit {
 
